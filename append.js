a2eb74052148273ac6ee72d4e03ba7fef68b8b64
Dishita28 <143996968+Dishita28@users.noreply.github.com>
2024-10-25 21:03:47 +0530

    Update append.js

--- append.js
+++ append.js
@@ -4,3 +4,11 @@ fs.appendFile('demo1.txt', 'Hello content!', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
+
+// open 
+var fs = require('fs');
+
+fs.open('demo1.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
