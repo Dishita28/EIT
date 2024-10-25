7c26197b47d06b7103a6b6735ce29c3f225aeed8
Dishita28 <143996968+Dishita28@users.noreply.github.com>
2024-10-25 21:47:29 +0530

    Update append.js

--- append.js
+++ append.js
@@ -12,3 +12,27 @@ fs.open('demo1.txt', 'w', function (err, file) {
   if (err) throw err;
   console.log('Saved!');
 });
+
+//write file
+var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+//delete
+var fs = require('fs');
+
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});
+
+//rename
+var fs = require('fs');
+
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
