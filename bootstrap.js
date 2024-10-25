968f704714463fd727856a75a90ee577303b4499
Dishita28 <143996968+Dishita28@users.noreply.github.com>
2024-10-25 21:04:20 +0530

    Create bootstrap.js

---
+++ bootstrap.js
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" initial-scale="1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    </head>
+    <body>
+        <h1>bootstrap 1</h1>
+
+        <!--fluid container-->>
+        <div class="container-fluid">
+            <p>dishita jeet</p>
+        </div>
+
+        <!--responsive container-->>
+        <div class="container-sm border">.container-sm</div>
+        <div class="container-md mt-3 border">.container-md</div>
+        <div class="container-lg mt-3 border">.container-lg</div>
+        <div class="container-xl mt-3 border">.container-xl</div>
+
+        <!--grid layout-->>
+        <div class="row">
+            <div class ="col-sm-4" style="background-color: red;">
+            hii dishita</div class>
+            <div class ="col-sm-4" style="background-color: blue">
+            hii shubh</div class>
+            <div class ="col-sm-4" style="background-color: green;">
+            hi jeet</div class>
+        </div>
+        
+    </body>
+</html>
